43eb18ee844a4821ec5f8bd6dbcb8c5c7619946a
berserkr1 <g.coirier@epmistes.net>
2022-10-17 11:03:55 +0200

    challenge quest submit

--- public/products.php
+++ public/products.php
@@ -3,11 +3,6 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
-// $name = 'Wilder';
-
-// echo $twig->render('home.html.twig', ['name' => $name]);
-
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 
 echo $twig->render('products.html.twig', compact('products'));
